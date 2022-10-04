d8963557e6cfc126c5f59a7eeee8d91e219bce94
Hannah Gibson <hgibson@actbluetech.com>
2022-10-04 17:26:33 -0400

    Use body-parser

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const body_parser = require('body-parser');
 
 const PORT = 8080;
 const app = express();
 
 app.set('view engine', 'pug');
+app.use(body_parser.urlencoded({extended: false})); // Use querystring library not qs library
 
 app.get('/', (request, response) => {
   response.redirect(303, '/card/0');
