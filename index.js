2228bc8cc57a85eaa8095e5223efbcd7340f2d1c
Hannah Gibson <hgibson@actbluetech.com>
2022-10-04 16:12:09 -0400

    Render index page with pug template

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require('express');
 const PORT = 8080;
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (request, response) => {
-  const body = 'Hello World!';
-  response.writeHead(200, {'Content-Type': 'text/plain', 'Content-Length': Buffer.byteLength(body)});
-  response.end(body);
+  response.render('index');
 });
 
 app.listen(PORT);
