b6fc54b11a9cd434b490e5eabd5c79fc1f21a314
Hannah Gibson <hgibson@actbluetech.com>
2022-12-21 12:56:58 -0500

    Improve logging

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/static', express.static('public'));
 
 // Logging middleware
 app.use((request, response, next) => {
-  console.log({'timestamp': Date.now(), 'message': `${request.method} ${request.url} ${response.statusCode}`});
+  console.log({'timestamp': Date.now(), 'severity': 'INFO', 'status': response.statusCode, 'message': `${request.method} ${request.url}`});
   next();
 });
 
@@ -50,18 +50,18 @@ app.use((request, response, next) => {
 
 // Error middleware
 app.use((error, request, response, next) => {
-  console.log({'timestamp': Date.now(), 'message': `${error.message} ${error.stack}`});
   error.status = error.status ? error.status : 500;
   response.status(error.status);
+  console.log({'timestamp': Date.now(), 'severity': 'ERROR', 'status': response.statusCode, 'message': `${error.message} ${error.stack}`});
   response.render('error', {error: error});
 });
 
 app.listen(PORT, async () => {
-  console.log({'timestamp': Date.now(), 'message': `App listening on port ${PORT}`});
+  console.log({'timestamp': Date.now(), 'severity': 'INFO', 'status': '', 'message': `App listening on port ${PORT}`});
   try {
     await DB.authenticate();
     await DB.sync();
   } catch (error) {
-    console.error('Error connecting to the database: ', error);
+    console.log({'timestamp': Date.now(), 'severity': 'ERROR', status: '', 'message': `${error.messagge} ${error.stack}`});
   }
 });
